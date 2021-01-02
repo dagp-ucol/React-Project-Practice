609660d6fdf275e7921a5acbf9dd479c67d51241
dagp-ucol <dgentner@ucol.mx>
2021-01-02 00:11:17 -0600

    added some properties

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,6 +3,8 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
+        <p class="aligncenter"><img align="center" width="180" height="180" alt="montser" src={`https://robohash.org/${props.monster.id}?set=set5`} /> </p>
         <h1> {props.monster.name} </h1>
+        <p> {props.monster.email} </p>
     </div>
 )
